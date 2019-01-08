890139fd4f50730eded10f34d87d317363d777cc
sabrina.moraru <sabrina.moraru@gmail.com>
2019-01-08 14:30:38 +0200

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+  return [
+    { title: "Javascript: The Good Parts", pages: 101 },
+    { title: "You Don't Know JS: Up & Going", pages: 90 },
+    { title: "Eloquent JavaScript", pages: 197 },
+    {
+      title: "JavaScript and JQuery: Interactive Front-End Web Development",
+      pages: 640
+    }
+  ];
+}
